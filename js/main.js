14b8aeee96b8f9efad3fe61ab928a8da3b452e7a
Sandy <peacesandy04@gmail.com>
2023-06-10 11:29:47 -0700

    A job listing page

--- js/main.js
+++ js/main.js
@@ -1,13 +1,67 @@
-document.querySelector('button').addEventListener('click', showMeData);
+//document.querySelector('button').addEventListener('click', showMeData);
 
-async function showMeData() {
-    const getData = async () => {
-        const response = await fetch('https://shesharpnl.github.io/hackathon-2023.sourcestack-data/assets/junior-nl.json');
-        const data = await response.json();
-        console.log(data)
-        return data;
-    }
-    let jsonData = await getData();
-    document.getElementById('output').innerHTML = jsonData.data[0].job_name;
-    document.getElementById('output2').innerHTML = jsonData.data[0].tag_categories[0];
-}
+//async function showMeData() {
+    //const getData = async () => {
+       // const response = await fetch('https://shesharpnl.github.io/hackathon-2023.sourcestack-data/assets/junior-nl-all-fields.json');
+       // const data = await response.json();
+       // console.log(data)
+        //return data;
+   // }
+    //let jsonData = await getData();
+   // document.getElementById('output').innerHTML = jsonData.data[0].job_name;
+    //document.getElementById('output2').innerHTML = jsonData.data[0].tag_categories[0];
+//}
+
+const wrapper = document.querySelector(".wrapper");
+const header = document.querySelector(".header");
+
+wrapper.addEventListener("scroll", (e) => {
+ e.target.scrollTop > 30
+  ? header.classList.add("header-shadow")
+  : header.classList.remove("header-shadow");
+});
+
+window.onload = function() {
+    var apiKey = prompt("Please enter your API Key:");
+    console.log(apiKey);
+    document.getElementById("apiKeyDisplay").textContent = "Submitted API Key: " + apiKey;
+  };
+
+const toggleButton = document.querySelector(".dark-light");
+
+toggleButton.addEventListener("click", () => {
+ document.body.classList.toggle("dark-mode");
+});
+
+const jobCards = document.querySelectorAll(".job-card");
+const logo = document.querySelector(".logo");
+const jobLogos = document.querySelector(".job-logos");
+const jobDetailTitle = document.querySelector(
+ ".job-explain-content .job-card-title"
+);
+const jobBg = document.querySelector(".job-bg");
+
+jobCards.forEach((jobCard) => {
+ jobCard.addEventListener("click", () => {
+  const number = Math.floor(Math.random() * 10);
+  const url = `https://unsplash.it/640/425?image=${number}`;
+  jobBg.src = url;
+
+  const logo = jobCard.querySelector("svg");
+  const bg = logo.style.backgroundColor;
+  console.log(bg);
+  jobBg.style.background = bg;
+  const title = jobCard.querySelector(".job-card-title");
+  jobDetailTitle.textContent = title.textContent;
+  jobLogos.innerHTML = logo.outerHTML;
+  wrapper.classList.add("detail-page");
+  wrapper.scrollTop = 0;
+ });
+});
+
+logo.addEventListener("click", () => {
+ wrapper.classList.remove("detail-page");
+ wrapper.scrollTop = 0;
+   jobBg.style.background = bg;
+});
+
